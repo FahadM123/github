7fff39b1c5efdb366a04f5b7a9cb12258cee9235
Malik <malik.fahad.mf20@gmail.com>
2023-03-08 10:37:30 -0800

    08-Mar-23 Added Abc Project

---
+++ AbcProject/src/AbcExample.java
@@ -0,0 +1,5 @@
+public class AbcExample {
+    public static void main(String[] args) {
+        System.out.println("My abc project journey starts");
+    }
+}
---
+++ CodingBat/src/Warmup1/SleepinClass.java
@@ -0,0 +1,14 @@
+package Warmup1;
+public static void  main(String[]args){
+        SleepIn(true,true);
+        SleepIn(true,false);
+        SleepIn(false,true);
+        SleepIn(false,false);
+
+        }
+public class SleepinClass {
+
+    //The parameter weekday is true if it is a weekday, and the parameter vacation is true if we are on vacation. We sleep in if it is not a weekday or we're on vacation. Return true if we sleep in
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+
+    }
