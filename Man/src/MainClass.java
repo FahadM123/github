637da65ff5e8676514e83f8eab2e1df2c6afa80b
malik.fahad <malik.fahad.mf20@gmail.com>
2023-03-23 16:46:53 -0700

    Mar 03-23-23 added ManClass

---
+++ Man/src/MainClass.java
@@ -0,0 +1,14 @@
+public class MainClass {
+    public static void main(String[] args) {
+         ManClass person1 = new ManClass();
+
+        person1.setName("Malik");
+        person1.setAge(27);
+
+        System.out.println(person1.getName());
+        System.out.println(person1.getAge());
+
+    }
+
+    }
+
